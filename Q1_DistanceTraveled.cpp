3bb1c83d22982e53e51d8c669b769d380870e332
Mvt1927 <viettheog4@gmail.com>
2023-03-07 18:52:07 +0700

    format code

--- Q1_DistanceTraveled.cpp
+++ Q1_DistanceTraveled.cpp
@@ -2,7 +2,7 @@
 #include "iomanip"
 using namespace std;
 
-void calcDistance(int speed,int time);
+void calcDistance(int speed, int time);
 bool checkInput(int inputValue);
 
 int main(int argc, char const *argv[])
@@ -13,46 +13,48 @@ int main(int argc, char const *argv[])
     cin >> speed;
     cout << "How many hours has it traveled? ";
     cin >> time;
-    if (checkInput(time)&&checkInput(speed))
+    if (checkInput(time) && checkInput(speed))
     {
-        calcDistance(speed,time);
-    } else cout << "No accept value < 1";
+        calcDistance(speed, time);
+    }
+    else
+        cout << "No accept value < 1";
     return 0;
 }
 
-void calcDistance(int speed, int time){
-    cout << setfill('-');	
+void calcDistance(int speed, int time)
+{
+    cout << setfill('-');
     cout << setw(45) << "-" << endl;
     cout << setfill(' ');
-    
+
     cout << setw(2) << left << "|";
     cout << setw(20) << left << "Time";
     cout << setw(1) << left << "|";
     cout << setw(20) << right << "Distance";
-    cout << setw(2) << right << "|"<<endl;
+    cout << setw(2) << right << "|" << endl;
 
-    cout << setfill('-');	
-    cout << setw(22)<< left << "|";
+    cout << setfill('-');
+    cout << setw(22) << left << "|";
     cout << setw(1) << "|";
     cout << setw(22) << right << "|" << endl;
     cout << setfill(' ');
     for (int i = 0; i < time; i++)
     {
         cout << setw(2) << left << "|";
-        cout << setw(20) << left << i+1;
+        cout << setw(20) << left << i + 1;
         cout << setw(1) << left << "|";
-        cout << setw(20) << right << (i+1)*speed;
-        cout << setw(2) << right << "|"<<endl;
-
+        cout << setw(20) << right << (i + 1) * speed;
+        cout << setw(2) << right << "|" << endl;
     }
-    cout << setfill('-');	
+    cout << setfill('-');
     cout << setw(45) << "-" << endl;
     cout << setfill(' ');
 }
 
 bool checkInput(int inputValue)
 {
-    if (inputValue<1)
+    if (inputValue < 1)
         return false;
     return true;
 }
